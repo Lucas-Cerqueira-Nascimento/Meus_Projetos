78bfce31b8a9cceb3d28ffb4500d69601d4ae487
lucas cerqueira do nascimento <terrarialucas123@gmail.com>
2025-08-18 08:44:30 -0300

    Fazendo alguns detalhes

--- PokeAPi/EventDOM.js
+++ PokeAPi/EventDOM.js
@@ -1,14 +1,15 @@
 // Sistema do Menu de tipos de Pokémons
 const menu = document.querySelector("#menu_type")
  const divType = document.querySelector(".pokeTypes")
-  const type = document.querySelector("#type")
+  const type = document.querySelector("#closeTab")
    let menuType = false
 menu.addEventListener("click", () => {
     if(!menuType){
-        menu.classList.add("rote_menu")
+        //menu.classList.add("rote_menu")
         divType.classList.add("On")
     type.addEventListener("click", () => {
         divType.classList.remove("On")
+        console.log("selecionou")
   }) 
  }
 })
