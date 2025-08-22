55a69155598950942c5359e899ed920038f1efdd
lucas cerqueira do nascimento <terrarialucas123@gmail.com>
2025-08-22 09:22:09 -0300

    Evento DOM

--- PokeAPi/EventDOM.js
+++ PokeAPi/EventDOM.js
@@ -9,9 +9,48 @@ menu.addEventListener("click", () => {
         divType.classList.add("On")
     type.addEventListener("click", () => {
         divType.classList.remove("On")
-        console.log("selecionou")
+        //console.log("selecionou")
   }) 
  }
 })
 
+// função para trocar de tipo de pokemon
+const pokeTypes = document.querySelectorAll(".types")
+let currentIndex = 0
+function AllpokesUpdate(index){
+    pokeTypes.forEach((pokes, i) => {
+        pokes.classList.toggle("On", i === index)
+    })
+}
 
+function changeTypes(value){
+    const numValue = (currentIndex) + value
+
+    if(numValue < 0 || numValue >= pokeTypes.length) return;
+
+    currentIndex = numValue
+
+    //console.log(currentIndex)
+
+    SelectTypePokemon(currentIndex)
+    AllpokesUpdate(currentIndex)
+}
+
+// Event SelectType of Pokemon
+const btn_selectType = document.querySelector("#selectType")
+
+function SelectTypePokemon(index){
+    btn_selectType.addEventListener("click", () => {
+        pokeTypes.forEach((poke, i) => {
+            if(i === index && poke.classList.contains("On")){
+                console.log(poke)
+            }
+        })
+    })
+}
+
+// Event back and next type 
+const BackArrow = document.querySelector("#back")
+BackArrow.addEventListener("click", () => changeTypes(-1))
+const NextArrow = document.querySelector("#next")
+NextArrow.addEventListener("click", () => changeTypes(1))
