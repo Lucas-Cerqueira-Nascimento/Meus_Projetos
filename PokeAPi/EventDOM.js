8d39c7e13d56cf59ab156e3dbbc2d981f7777b33
lucas cerqueira do nascimento <terrarialucas123@gmail.com>
2025-08-23 17:40:25 -0300

    Atualizando

--- PokeAPi/EventDOM.js
+++ PokeAPi/EventDOM.js
@@ -44,6 +44,7 @@ function SelectTypePokemon(index){
         pokeTypes.forEach((poke, i) => {
             if(i === index && poke.classList.contains("On")){
                 divType.classList.remove("On")
+                ApiFetch(index)
             }
         })
     })
@@ -54,3 +55,37 @@ const BackArrow = document.querySelector("#back")
 BackArrow.addEventListener("click", () => changeTypes(-1))
 const NextArrow = document.querySelector("#next")
 NextArrow.addEventListener("click", () => changeTypes(1))
+
+// Api do tipo de pokemon
+async function ApiFetch(Num){ 
+
+    const res = await fetch(`https://pokeapi.co/api/v2/type/${Num}`)
+
+    const dados = await res.json()
+
+    console.log(dados.name)
+}
+
+async function Test(){
+    const res = await fetch("https://pokeapi.co/api/v2/type/1")
+    const dados = await res.json()
+
+    //console.log(dados.pokemon[0].pokemon.name) //Acessa o obj do pokemon dentro do array 
+    
+    //console.log(dados["pokemon"][0]["pokemon"]["name"])
+
+
+    const Allnames = dados.pokemon //Acessa todos os pokemons
+    //console.log(Allnames)
+
+    Allnames.forEach((pokes, i) => {
+        //console.log(pokes) Objetos
+        //console.log(i) Chave dos pokemons
+
+        // let test = dados.pokemon[i].pokemon.name
+
+        // console.log(test) //Saída: Todos os nomes dos Pokemons!
+    })
+}
+
+Test()
