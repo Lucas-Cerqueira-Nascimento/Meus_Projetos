14108a973d434d1020cad53e02c7b2ab2a2d8d41
lucas cerqueira do nascimento <terrarialucas123@gmail.com>
2025-09-29 07:39:52 -0300

    Update TaskPage.jsx

--- TasksLists/src/components/Pages/TaskPage.jsx
+++ TasksLists/src/components/Pages/TaskPage.jsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon } from "lucide-react";
 import { useSearchParams } from "react-router";
 
 function TaskPage() {
@@ -6,8 +7,16 @@ function TaskPage() {
   const description = searchParams.get("description");
   return (
     <div className="w-screen h-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <h1 className="flex justify-center text-3xl text-slate-100 font-bold text-center title">
+        <ChevronLeftIcon />
+        Detalhes da tarefa
+      </h1>
+      <div className="text-left bg-slate-400 w-full text-white p-2 rounded-md lists">
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow border">
+          <h2 className="text-slate-700 text-2xl">{title}</h2>
+          <p className="text-slate-700">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
